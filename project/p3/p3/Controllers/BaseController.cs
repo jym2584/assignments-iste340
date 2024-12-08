e10b8d35452d9d938490f6d38952ba00576d8541
Jin Moon <jym2584@rit.edu>
2024-12-07 21:54:13 -0500

    Done with project for now

---
+++ project/p3/p3/Controllers/BaseController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using p3.Models;
+using p3.Services;
+
+public class BaseController : Controller
+{
+
+    public BaseController()
+    {
+
+    }
+
+    public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        var dr = new DataRetrieval();
+
+        var footerData = await dr.GetData("footer/");
+        var footerModel = JsonConvert.DeserializeObject<FooterModel>(footerData);
+
+        ViewData["FooterModel"] = footerModel;
+        await base.OnActionExecutionAsync(context, next);
+    }
+}
--- project/p3/p3/wwwroot/js/site.js
+++ project/p3/p3/wwwroot/js/site.js
@@ -13,6 +13,12 @@ $(document).ready(function () {
         height: 300
     });
 
+    $(".rotate").textrotator({
+        animation: "dissolve", // You can pick the way it animates when rotating through words. Options are dissolve (default), fade, flip, flipUp, flipCube, flipCubeUp and spin.
+        separator: ",", // If you don't want commas to be the separator, you can define a new separator (|, &, * etc.) by yourself using this field.
+        speed: 2000 // How many milliseconds until the next word show.
+    });
+
     $('#programAcc').accordion({ collapsible: true });
     $('#degreeAcc').accordion({ collapsible: true });
     $('#gradDegreeAcc').accordion({ collapsible: true });
